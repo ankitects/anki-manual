7343b32559f4dfbae9522724be8ae7263f794c7a
GithubAnon0000 <160563432+GithubAnon0000@users.noreply.github.com>
2025-06-05 08:30:00 +0000

    ADD support for admonitions (#403)
    * ADD support for admonitions
    
    * Add an example so the PR can be tested
    
    * mdbook-admonish needs to be installed
    
    ---------
    
    Co-authored-by: Damien Elmes <gpg@ankiweb.net>

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,6 +4,7 @@
 MDBOOK_VERSION="0.4.51"
 MDBOOK_LINKCHECK_COMMIT="3222e857f22a5b41596070848203d160a4d78fd9"
 MDBOOK_TOC_VERSION="0.14.2"
+MDBOOK_ADMONISH_VERSION="1.19.0"
 
 # mdbook needs to be able to find things like mdbook-toc
 export PATH="$HOME/.cargo/bin:$PATH"
@@ -26,6 +27,7 @@ install_if_needed() {
 install_if_needed "mdbook" "cargo install mdbook --version $MDBOOK_VERSION"
 install_if_needed "mdbook-toc" "cargo install mdbook-toc --version $MDBOOK_TOC_VERSION"
 install_if_needed "mdbook-linkcheck" "cargo install --git https://github.com/ankitects/mdbook-linkcheck --rev $MDBOOK_LINKCHECK_COMMIT mdbook-linkcheck"
+install_if_needed "mdbook-admonish" "cargo install mdbook-admonish --version $MDBOOK_ADMONISH_VERSION"
 
 # Avoid checking links in deploy mode
 if [ "$CHECK" == "" ]; then
